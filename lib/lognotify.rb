bdee4f3b98a20ef926ef67efdd015c299b670220
Your Name <you@example.com>
2015-10-04 11:04:05 +0100

    1st commit

---
+++ lib/lognotify.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+# file: lognotify.rb
+
+require 'sps-pub'
+
+
+class LogNotify
+
+  def initialize(logfile, sps_address: nil, sps_port: 59000, sps_topic: 'lognotify')
+
+    @sps = sps_address ? SPSPub.new(address: sps_address, port: sps_port) : nil
+    @sps_topic = sps_topic
+
+    @command = 'tail -n 1 -f ' + logfile
+
+  end
+  
+  def on_update(entry)
+    
+    # custom defined
+    
+  end
+
+  def start()
+
+    t = Time.now # using the time we can ignore existing entries
+
+    IO.popen(@command).each_line do |x| 
+      
+      # anything after 5 seconds from start is new
+      if Time.now > t + 5 then 
+        
+        raw_log_entry = x.lines.last
+        
+        #@sps.notice(@sps_topic + ': ' + json) if @sps
+        on_update(raw_log_entry)
+      end
+    end
+
+  end  
+
+end
+
+if __FILE__ == $0 then
+  
+  ln = LogNotify.new('/var/log/messages')
+
+  def ln.on_update(entry)
+
+    puts 'entry: ' + entry.inspect
+    
+  end
+
+  ln.start
+  
+end
---
+++ lognotify.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'lognotify'
+  s.version = '0.1.0'
+  s.summary = 'Actively monitors a log file for new entries and can trigger an event using a regular expression'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/lognotify.rb']
+  s.add_runtime_dependency('sps-pub', '~> 0.4', '>=0.4.0') 
+  s.signing_key = '../privatekeys/lognotify.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/lognotify'
+end
